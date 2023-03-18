dec1dadbe6d3c8d828919c05811b8e0881999bb7
nike-getto <100715230+nike-getto@users.noreply.github.com>
2023-03-18 19:43:38 +0100

    Update App.js
    added basic functionality

--- src/App.js
+++ src/App.js
@@ -1,36 +1,54 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
+import { Alchemy, Network } from 'alchemy-sdk'
+import { useEffect, useState } from 'react'
 
-import './App.css';
+import './App.css'
 
 // Refer to the README doc for more information about using API
 // keys in client-side code. You should never do this in production
 // level code.
 const settings = {
-  apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
-  network: Network.ETH_MAINNET,
-};
-
+	apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+	network: Network.ETH_MAINNET,
+}
 
 // In this week's lessons we used ethers.js. Here we are using the
 // Alchemy SDK is an umbrella library with several different packages.
 //
 // You can read more about the packages here:
 //   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
-const alchemy = new Alchemy(settings);
+const alchemy = new Alchemy(settings)
 
 function App() {
-  const [blockNumber, setBlockNumber] = useState();
+	const [blockNumber, setBlockNumber] = useState()
+	const [nonce, setNonce] = useState()
+	const [hash, setHash] = useState()
+	const [gasLimit, setGasLimit] = useState()
 
-  useEffect(() => {
-    async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
-    }
+	useEffect(() => {
+		async function getBlockNumber() {
+			setBlockNumber(await alchemy.core.getBlockNumber())
+		}
 
-    getBlockNumber();
-  });
+		getBlockNumber()
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+		async function getBlock() {
+			const result = await alchemy.core.getBlock(blockNumber)
+			setNonce(parseInt(result.nonce, 16))
+			setHash(result.hash)
+			setGasLimit(parseInt(result.gasLimit._hex, 16))
+		}
+
+		getBlock()
+	})
+
+	return (
+		<div className='App'>
+			Block Number: {blockNumber} <br />
+			Nonce: {nonce} <br />
+			Hash: {hash} <br />
+			Gas Limit: {gasLimit} <br />
+		</div>
+	)
 }
 
-export default App;
+export default App
